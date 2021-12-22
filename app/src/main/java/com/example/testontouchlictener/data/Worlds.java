dc5fe2c79f647ed7092f2c036b437ceea3cf52c7
Максим Толстобров <maximah1989@yandex.ru>
2021-12-22 15:17:46 +0300

    Update

--- app/src/main/java/com/example/testontouchlictener/data/Worlds.java
+++ app/src/main/java/com/example/testontouchlictener/data/Worlds.java
@@ -7,14 +7,30 @@ import java.util.List;
 
 public class Worlds {
 
+    private int idWorld;
     private String nameWorld;
     private int maxLevelInWorld;
     private int bgImage;
 
-    public Worlds(String nameWorld, int maxLevelInWorld, int bgImage) {
+    public Worlds(int idWorld, String nameWorld, int maxLevelInWorld, int bgImage) {
+        this.idWorld = idWorld;
         this.nameWorld = nameWorld;
         this.maxLevelInWorld = maxLevelInWorld;
         this.bgImage = bgImage;
+    }
+
+    public static List<Worlds> getWorlds() {
+        List<Worlds> worlds = new ArrayList<>();
+
+        worlds.add(new Worlds(1, "Мифы древней Греции", 100, R.drawable.test_resycler_1));
+        worlds.add(new Worlds(2, "Океания", 100, R.drawable.test_resycler_2));
+        worlds.add(new Worlds(3, "Космос", 100, R.drawable.test_resycler_3));
+
+        return worlds;
+    }
+
+    public int getIdWorld() {
+        return idWorld;
     }
 
     public String getNameWorld() {
@@ -27,15 +43,5 @@ public class Worlds {
 
     public int getBgImage() {
         return bgImage;
-    }
-
-    public static List<Worlds> getWorlds(){
-        List<Worlds> worlds = new ArrayList<>();
-
-        worlds.add(new Worlds("Мифы древней Греции", 100, R.drawable.test_resycler_1));
-        worlds.add(new Worlds("Океания", 100, R.drawable.test_resycler_2));
-        worlds.add(new Worlds("Космос", 100, R.drawable.test_resycler_3));
-
-        return worlds;
     }
 }
